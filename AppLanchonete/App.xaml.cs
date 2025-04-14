e2c0a440d4951c9b664d2d9b4f8b508b9e1ddb3f
Leandro Amaral <lramaral02m@gmail.com>
2025-04-14 14:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ AppLanchonete/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppLanchonete
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
